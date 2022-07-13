fcce4ac6534e6ce0ef5ee012226ef39496c63b49
Taylor Gray <taaylorgraay@gmail.com>
2022-07-13 12:32:32 +0100

    set up server with endpoints, routes and controllers

---
+++ bwm-new/server/routes/rentals.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const { 
+  getRentals,
+  getRentalById,
+  createRental,
+  updateRental,
+  deleteRental } = require('../controllers/rentals');
+
+router.get('', getRentals);
+router.get('/:rentalId', getRentalById);
+router.post('', createRental);
+router.delete('/:id', deleteRental);
+router.patch('/:id', updateRental);
+
+module.exports = router;
