0f085abd876710f81d478f62c500a34a698874fb
Taylor Gray <taaylorgraay@gmail.com>
2022-07-13 18:06:12 +0100

    implement rental-listing component with styling

--- bwm-new/src/app/rental/rental.module.ts
+++ bwm-new/src/app/rental/rental.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 import { RentalDetailComponent } from './rental-detail/rental-detail.component';
 import { RentalListingComponent } from './rental-listing/rental-listing.component';
 import { RentalComponent } from './rental.component';
+
+import { RentalService } from './shared/rental.service';
 
 const routes: Routes = [
   {
@@ -22,8 +25,12 @@ const routes: Routes = [
     RentalListingComponent,
     RentalComponent
   ],
+  providers: [
+    RentalService
+  ],
   imports: [
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    CommonModule
   ]
 })
-export class RentalModule {} 
+export class RentalModule {}
