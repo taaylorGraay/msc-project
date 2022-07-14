2fa5422577b6e6f310abaf20e928df8723a9c7df
Taylor Gray <taaylorgraay@gmail.com>
2022-07-14 14:25:00 +0100

    validate register form

--- bwm-new/src/app/auth/register/register.component.ts
+++ bwm-new/src/app/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControlName, NgForm } from '@angular/forms';
+import { RegisterForm } from '../shared/register-form.model';
 
 @Component({
   selector: 'bwm-register',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  registerFormData!: RegisterForm;
+  emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
   constructor() { }
 
   ngOnInit() {
+    this.registerFormData = new RegisterForm();
+  }
+
+  register(form: NgForm) {
+    this.validateInputs(form);
+
+    if(form.invalid) { return; }
+
+    alert(this.diagnostic);
+  }
+
+  validateInputs(form:NgForm){
+    Object.keys(form.controls).forEach(controlName =>{
+      form.controls[controlName].markAsDirty();
+    })
+  }
+
+  get diagnostic(): string {
+    return JSON.stringify(this.registerFormData);
   }
 
 }
