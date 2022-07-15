03a223ad2c57845e956390421a4bbd7411a8def2
Taylor Gray <taaylorgraay@gmail.com>
2022-07-15 17:52:00 +0100

    try to solve hooks error

--- bwm-react-new/src/store/index.js
+++ bwm-react-new/src/store/index.js
@@ -1,11 +1,13 @@
 import { legacy_createStore, combineReducers } from 'redux';
 import rentals from './reducers/rentals';
+import rental from './reducers/rental';
 
 export function initStore() {
   // PURE Functions, TODO: Explain (:
   const reducers = combineReducers({
-    rentals
-  })
+    rentals,
+    rental
+  });
   
   const reduxExtension = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
   const store = legacy_createStore(reducers, reduxExtension);
