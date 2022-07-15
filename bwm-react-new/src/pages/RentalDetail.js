b18366f8bc56a62d435c3760af14802158042992
Taylor Gray <taaylorgraay@gmail.com>
2022-07-15 19:00:54 +0100

    fix hooks error to fetch data and implement a helper function

--- bwm-react-new/src/pages/RentalDetail.js
+++ bwm-react-new/src/pages/RentalDetail.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { fetchRentalById } from '../actions';
+import React from "react";
+import { connect } from "react-redux";
+import { fetchRentalById } from "../actions";
+import { capitalize } from '../helpers/functions';
 
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+
+function withRouter(Component) {
+  function ComponentWithRouterProp(props) {
+    let location = useLocation();
+    let navigate = useNavigate();
+    let params = useParams();
+    return <Component {...props} router={{ location, navigate, params }} />;
+  }
+
+  return ComponentWithRouterProp;
+}
 
 class RentalDetail extends React.Component {
-
   componentDidMount() {
-    const { id } = this.props.match.params;
-    this.props.dispatch(fetchRentalById(id))
+    const {id} = this.props.router.params;
+    this.props.dispatch(fetchRentalById(id));
   }
 
   render() {
@@ -34,37 +45,55 @@ class RentalDetail extends React.Component {
               <div className="rental">
                 {/* <!-- TODO: Display shared category --> */}
                 <h2 className={`rental-type type-${rental.category}`}>
-                  {rental.shared ? 'Shared' : 'Whole'} {rental.category}
+                  {rental.shared ? "Shared" : "Whole"} {rental.category}
                 </h2>
                 {/* <!-- TODO: Display title --> */}
                 <h1 className="rental-title">{rental.title}</h1>
                 {/* <!-- TODO: Display city --> */}
-                <h2 className="rental-city">{rental.city}</h2>
+                <h2 className="rental-city">{capitalize(rental.city)}</h2>
                 <div className="rental-room-info">
                   {/* <!-- TODO: Display numOfRooms --> */}
-                  <span><i className="fa fa-building"></i>{rental.numOfRooms} bedrooms</span>
-                    {/* // <!-- TODO: Display numOfRooms + 4 --> */}
-                  <span><i className="fa fa-user"></i> {rental.numOfRooms + 4} guests</span>
+                  <span>
+                    <i className="fa fa-building"></i>
+                    {rental.numOfRooms} bedrooms
+                  </span>
+                  {/* // <!-- TODO: Display numOfRooms + 4 --> */}
+                  <span>
+                    <i className="fa fa-user"></i> {rental.numOfRooms + 4}{" "}
+                    guests
+                  </span>
                   {/* // <!-- TODO: Display numOfRooms + 2 --> */}
-                  <span><i className="fa fa-bed"></i> {rental.numOfRooms + 2} beds</span>
+                  <span>
+                    <i className="fa fa-bed"></i> {rental.numOfRooms + 2} beds
+                  </span>
                 </div>
                 {/* <!-- TODO: Display description --> */}
-                <p className="rental-description">
-                  {rental.description}
-                </p>
-                <hr/>
+                <p className="rental-description">{rental.description}</p>
+                <hr />
                 <div className="rental-assets">
                   <h3 className="title">Assets</h3>
                   <div className="row">
                     <div className="col-md-6">
-                      <span><i className="fa fa-asterisk"></i> Cooling</span>
-                      <span><i className="fa fa-thermometer"></i> Heating</span>
-                      <span><i className="fa fa-location-arrow"></i> Iron</span>
+                      <span>
+                        <i className="fa fa-asterisk"></i> Cooling
+                      </span>
+                      <span>
+                        <i className="fa fa-thermometer"></i> Heating
+                      </span>
+                      <span>
+                        <i className="fa fa-location-arrow"></i> Iron
+                      </span>
                     </div>
                     <div className="col-md-6">
-                      <span><i className="fa fa-desktop"></i> Working area</span>
-                      <span><i className="fa fa-cube"></i> Washing machine</span>
-                      <span><i className="fa fa-cube"></i> Dishwasher</span>
+                      <span>
+                        <i className="fa fa-desktop"></i> Working area
+                      </span>
+                      <span>
+                        <i className="fa fa-cube"></i> Washing machine
+                      </span>
+                      <span>
+                        <i className="fa fa-cube"></i> Dishwasher
+                      </span>
                     </div>
                   </div>
                 </div>
@@ -73,12 +102,12 @@ class RentalDetail extends React.Component {
             <div className="col-md-4"> BOOKING</div>
           </div>
         </div>
-      </section> 
-    )
+      </section>
+    );
   }
 }
 
-const mapStateToProps = ({rental}) => ({ rental })
+const mapStateToProps = ({ rental }) => ({ rental });
 
-const RentalDetailWithRouter = useNavigate(RentalDetail);
+const RentalDetailWithRouter = withRouter(RentalDetail);
 export default connect(mapStateToProps)(RentalDetailWithRouter);
