a12c6a1adaf20e6e65dc0e4db8220c27c5f8423a
Taylor Gray <taaylorgraay@gmail.com>
2022-07-17 14:49:26 +0100

    complete login and register forms with validation

--- bwm-react-new/src/components/forms/LoginForm.js
+++ bwm-react-new/src/components/forms/LoginForm.js
@@ -1,53 +1,45 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { ErrorMessage } from '@hookform/error-message';
+import FormError from './FormError';
 
-const EMAIL_PATTERN = /^(([^<>()\\[\]\\.,;:\s@"]+(\.[^<>()\\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const Error = ({ children }) =>
+  <div className="alert alert-danger">
+    {children}
+  </div>
 
-const LoginForm = ({onSubmit}) => {
+// eslint-disable-next-line
+const EMAIL_PATTERN = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  const { register, handleSubmit, errors } = useForm();
+const LoginForm = ({ onSubmit }) => {
+
+  const { register, handleSubmit, formState: { errors } } = useForm();
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="form-group">
         <label htmlFor="email">Email</label>
-        <input
-          {...register({required: true, pattern: EMAIL_PATTERN})}
+        <input {...register("email", { required: 'Email is required', pattern: { value: EMAIL_PATTERN, message: 'Invalid email format!' } })}
           name="email"
           type="email"
           className="form-control"
           id="email" />
-        { errors.email &&
-          <div className="alert alert-danger"> 
-            { errors.email.type === 'required' &&
-              <span>Email is required!</span>
-            }
-            { errors.email.type === 'pattern' &&
-              <span>Not valid email format!</span>
-            }
-          </div>
-        }
+        <FormError errors={errors} name="email">
+          {(message) => <p>{message}</p>}
+        </FormError>
       </div>
       <div className="form-group">
         <label htmlFor="password">Password</label>
-        <input 
-          {...register({required: true, minLength: 8})}
+        <input {...register("password", { required: "Password is required!", minLength: { value: 8, message: 'Minimum length of password is 8 characters!' } })}
           name="password"
           type="password"
           className="form-control"
           id="password" />
-        { errors.password &&
-          <div className="alert alert-danger"> 
-            { errors.password.type === 'required' &&
-              <span>Password is required!</span>
-            }
-            { errors.password.type === 'minLength' &&
-              <span>Minimum length of password is 8 characters!</span>
-            }
-          </div>
-        }
+        <ErrorMessage as={<Error />} errors={errors} name="password">
+          {({ message }) => <p>{message}</p>}
+        </ErrorMessage>
       </div>
-      <button 
-        type="submit" 
+      <button
+        type="submit"
         className="btn btn-bwm-main">Submit</button>
     </form>
   )
